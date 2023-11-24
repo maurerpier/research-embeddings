b30c15307240750e081ac3ea9d1af3a49680b9d1
magic <samsgrice@gmail.com>
2023-11-24 10:28:13 +0100

    updated data functions

--- data.py
+++ data.py
@@ -6,7 +6,7 @@ from time import time
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-def process_dataset(path):
+def process_dataset(path, processed_path=""):
     # load data
     df = pd.read_csv(path)
     df = df[["topic_body", "topics"]].rename(columns={"topic_body": "text"}).dropna()
@@ -27,8 +27,9 @@ def process_dataset(path):
             df.loc[idx, "multi-topic"] = True
     df["multi-topic"].fillna(False, inplace=True)
     df = df.reset_index(drop=True)
+    if processed_path:
+        df.to_csv(processed_path, index=False)
     return df
-
 
 def tokenize(string, word_level=True, consecutative_tokens=1):
     if word_level:
@@ -38,21 +39,45 @@ def tokenize(string, word_level=True, consecutative_tokens=1):
         tokens = [string[i: i + consecutative_tokens] for i in range(0, len(string) - consecutative_tokens)]
         return tokens
 
-def vectorize(tokened_sentances):
+def vocabulary(tokened_sentances):
     VOCAB = ['_PAD', '_GO', '_EOS', '_UNK']
     unique_tokens = set()
 
+    for tokens in tokened_sentances:
+        unique_tokens.update(tokens)
+    VOCAB += sorted(unique_tokens)
+
+    vocab_map = {}
+    for i, token in enumerate(VOCAB):
+        vocab_map[token] = i
+
+    # vocab_map, unique tokens
+    return vocab_map, VOCAB
+
+def vectorize(sentances, vocab_map, word_level, max_length=100):
+    n_sequences = len(sentances)
+    vectors = np.empty(shape=(n_sequences, max_length), dtype=np.int32)
+    vectors.fill(vocab_map["_PAD"])
+    for i, sentance in enumerate(sentances):
+        tokens = tokenize(sentance, word_level=word_level)
+        vectors[i, -len(tokens):] = [vocab_map.get(t, '_UNK') for t in tokens]
+    return vectors
 
 def vectorize_df(df):
-    tokens = df["text"].apply(tokenize)
+    df["tokens"] = df["text"].apply(tokenize)
+    vocab_map, vocab = vocabulary(df["tokens"])
+    max_length = df["tokens"].apply(len).max()
+    vectors = vectorize(df["text"], vocab_map, True, max_length)
+    return vectors
 
 
 if __name__ == "__main__":
     dataset_path = "Reuters/reuters21578_news.csv"
+    processed_path = "text_data.csv"
 
-    ts = time()
-    df = process_dataset(dataset_path)
-    te = time()
-    print("preprocessing took %2.4f sec" % (te - ts))
+    # df = process_dataset(dataset_path, processed_path)
 
-    vectorize_df(df)
+    df = pd.read_csv(processed_path)
+    max_words = 400
+    df = df[(df["text"].apply(tokenize).apply(len) <= max_words)]
+    vectors = vectorize_df(df)
